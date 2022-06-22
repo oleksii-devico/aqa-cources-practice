b1aab45f7ff4bc5b8e3c7444c6399420dc775fdc
Oleksii <oleksii.kharchenko@devico.io>
2022-06-22 12:50:15 +0300

    add function and function expression

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+function testReassignment(){
+    console.log('I am the function!')
+}
+testReassignment();
+testReassignment = 2 //checking that function could be reassigned
+console.log(testReassignment) //output '2'
 
+const logHelloWorldWithAdjective = (adj) => {
+    console.log(`Hello ${adj} World!`)
+}
+logHelloWorldWithAdjective('JS')
+
+logHelloWorldWithAdjective = 2 //expected output is TypeError since we declaring function with const keyword
+console.log(logHelloWorldWithAdjective)
