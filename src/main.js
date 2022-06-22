c3db352933cee4a2453aaea67da923a88625c22f
Oleksii <oleksii.kharchenko@devico.io>
2022-06-22 14:44:23 +0300

    asd

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ function testReassignment(){
     console.log('I am the function!')
 }
 testReassignment();
+console.log(testReassignment)
 testReassignment = 2 //checking that function could be reassigned
-console.log(testReassignment) //output '2'
+console.log(`I was a function before, but now I'm '${testReassignment}'`) 
 
 const logHelloWorldWithAdjective = (adj) => {
     console.log(`Hello ${adj} World!`)
