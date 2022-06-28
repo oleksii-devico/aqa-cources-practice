3c1dd42f0d2cfc7fbb0f5026e6feedbe04410d74
Oleksii <oleksii.kharchenko@devico.io>
2022-06-28 19:48:42 +0300

    update tests

--- cypress/e2e/created specs/tests from cypress io.cy.js
+++ cypress/e2e/created specs/tests from cypress io.cy.js
@@ -3,6 +3,9 @@
 describe('My First Test', () => {
   it('Gets, types and asserts', () => {
     cy.visit('https://example.cypress.io')
-    cy.contains('get').click()
+    cy.contains('get').click();
+    cy.get('h1').should('be.visible').should('have.text', 'Querying');
+    cy.get('#query-btn').should('contain', 'Button').click()
+    cy.get('[data-cy="submit"]').click()
   })
 })
