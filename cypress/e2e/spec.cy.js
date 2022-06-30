e519af1bb0cbd7502fe955a5f8d4964e65eb383a
Oleksii <oleksii.kharchenko@devico.io>
2022-06-30 13:55:30 +0300

    move spec to created specs folder

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('example.cypress.io')
-    cy.get('.caret').click()
-    cy.get('.dropdown-menu > :nth-child(5) > a').click()
-    cy.get('h1').should('have.text', 'Viewport')
-    
-  })
-})
