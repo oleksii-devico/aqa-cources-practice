087c8408af6baaaa673f1336bd1e2e3a23b61fc2
Oleksii <oleksii.kharchenko@devico.io>
2022-07-01 16:37:10 +0300

    playing around js

---
+++ src/average.js
@@ -0,0 +1,11 @@
+
+const getAverageNum = (numbersArr) => {
+    let numbersSum = 0
+    numbersArr.forEach(element => {
+        numbersSum += element
+    })
+    return numbersSum / numbersArr.length
+};
+const numbers = [5,1]
+
+console.log(getAverageNum(numbers))
