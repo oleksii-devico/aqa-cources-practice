dd49303d86ea68c4a31c313fd3312ce0b14d917d
Oleksii <oleksii.kharchenko@devico.io>
2022-07-06 14:55:28 +0300

    refactor lmgfu spec

--- cypress/e2e/3-created-specs/lmgfu.cy.js
+++ cypress/e2e/3-created-specs/lmgfu.cy.js
@@ -1,6 +1,9 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://google.com')
-    cy.get('.gLFyf').type('Аниме онлайн без регистрации и смс {enter}')
+    const addEnterToString = (string) => {
+      return string += `{enter}`
+    }
+    cy.get('.gLFyf').type(addEnterToString('Аниме онлайн без регистрации и смс'))
   })
 })
