ebc08625aa68f791cf2b0c1cf2a3e111d9008a65
Oleksii <oleksii.kharchenko@devico.io>
2022-07-12 15:01:59 +0300

    remove extra empty line

--- cypress/e2e/3-created-specs/spec.cy.js
+++ cypress/e2e/3-created-specs/spec.cy.js
@@ -4,6 +4,5 @@ describe('empty spec', () => {
     cy.get('.caret').click()
     cy.get('.dropdown-menu > :nth-child(5) > a').click()
     cy.get('h1').should('have.text', 'Viewport')
-    
   })
 })
