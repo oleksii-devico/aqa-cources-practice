84b206225ff6780cb2ac8e26990e259a7b0c6af0
Oleksii <oleksii.kharchenko@devico.io>
2022-07-14 21:05:20 +0300

    move playground specs to separate branch

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'vixnrq',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  //viewportHeight: 1080,
-  //viewportWidth: 1920
 });
--- cypress/e2e/3-created-specs/lmgfu.cy.js
+++
@@ -1,9 +0,0 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://google.com')
-    const addEnterToString = (string) => {
-      return string += `{enter}`
-    }
-    cy.get('.gLFyf').type(addEnterToString('Аниме онлайн без регистрации и смс'))
-  })
-})
