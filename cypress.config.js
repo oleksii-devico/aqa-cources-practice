b8a1fe3000cda6f292998b4a9075c79c9ddeb3e6
Oleksii <oleksii.kharchenko@devico.io>
2022-07-14 21:44:34 +0300

    update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    watchForFileChanges: true,
+    numTestsKeptInMemory: 50,
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    retries: {
+      runMode: 2,
+      openMode: 1,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
     },
   },
 });
