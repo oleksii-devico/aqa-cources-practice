9f8d0ec72fb0ca12dffd83d9655573caef79d69b
Oleksii Kharchenko <oleksii.kharchenko@devico.io>
2022-07-25 17:53:58 +0300

    formatting

--- cypress/e2e/sign_in_page_ui_tests.cy.js
+++ cypress/e2e/sign_in_page_ui_tests.cy.js
@@ -1,66 +1,81 @@
 /// <reference types="cypress" />
 import { sign_in_page } from "../selectors/sign_in_page";
 
-describe('UI tests for sign in page', () => {
-
-  before('visiting sign in page', () => {
-    cy.visit('/')
-  })
+describe("UI tests for sign in page", () => {
+  before("visiting sign in page", () => {
+    cy.visit("/");
+  });
 
   it('should show "Real World App logo"', () => {
-    cy.get(sign_in_page.logo_image).should('be.visible').and('have.attr', 'xmlns', 'http://www.w3.org/2000/svg')
-  })
+    cy.get(sign_in_page.logo_image)
+      .should("be.visible")
+      .and("have.attr", "xmlns", "http://www.w3.org/2000/svg");
+  });
 
   it('should show "Sign in" title', () => {
-    cy.get(sign_in_page.title_text).should('be.visible').and('have.text', 'Sign in')
-  })
+    cy.get(sign_in_page.title_text)
+      .should("be.visible")
+      .and("have.text", "Sign in");
+  });
 
-  it('should show typeable Username field', () => {
-    cy.get(sign_in_page.username_field).should('be.visible').type('asdasd').clear()
-  })
+  it("should show typeable Username field", () => {
+    cy.get(sign_in_page.username_field)
+      .should("be.visible")
+      .type("asdasd")
+      .clear();
+  });
 
-  it('should show typeable Password field', () => {
-    cy.get(sign_in_page.password_field).should('be.visible').type('asdasd').clear()
-  })
+  it("should show typeable Password field", () => {
+    cy.get(sign_in_page.password_field)
+      .should("be.visible")
+      .type("asdasd")
+      .clear();
+  });
 
-  it('should show Username and Password placeholders', () => {
-    cy.get(sign_in_page.username_title).should('have.text', 'Username')
-    cy.get(sign_in_page.password_title).should('have.text', 'Password')
-  })
+  it("should show Username and Password placeholders", () => {
+    cy.get(sign_in_page.username_title).should("have.text", "Username");
+    cy.get(sign_in_page.password_title).should("have.text", "Password");
+  });
 
   it("should show 'Username is required' error if user clicks on it and then click outside this field and didn't enter any value", () => {
-    cy.reload()
-    cy.get(sign_in_page.username_validation_message).should('not.exist')
-    cy.get(sign_in_page.username_field).click().blur()
-    cy.get(sign_in_page.username_validation_message).should('be.visible')
-  })
+    cy.reload();
+    cy.get(sign_in_page.username_validation_message).should("not.exist");
+    cy.get(sign_in_page.username_field).click().blur();
+    cy.get(sign_in_page.username_validation_message).should("be.visible");
+  });
 
   it('check "Remember me" checkbox', () => {
-    cy.get(sign_in_page.rememberMe_checkbox).should('not.be.checked')
-      .check().should('be.checked')
-  })
+    cy.get(sign_in_page.rememberMe_checkbox)
+      .should("not.be.checked")
+      .check()
+      .should("be.checked");
+  });
 
-  it('should show disabled by default sign in btn', () => {
-    cy.reload()
-    cy.get(sign_in_page.signIn_button).should('be.disabled') //this button is enabled by default in the app so test fails :)
-  })
+  it("should show disabled by default sign in btn", () => {
+    cy.reload();
+    cy.get(sign_in_page.signIn_button).should("be.disabled"); //this button is enabled by default in the app so test fails :)
+  });
 
   it("should have 'Don't have an account? Sign Up' clickable link under 'Sign in' btn", () => {
-    cy.get(sign_in_page.signUp_link).should('have.text', "Don't have an account? Sign Up").and('have.attr', 'href', '/signup')
-  })
+    cy.get(sign_in_page.signUp_link)
+      .should("have.text", "Don't have an account? Sign Up")
+      .and("have.attr", "href", "/signup");
+  });
 
   it("should show Cypress copyright link that leads to 'https://www.cypress.io/'", () => {
-    cy.get(sign_in_page.cypress_copyright).should('have.attr', 'href', 'https://cypress.io')
-      .and('have.attr', 'target', '_blank') //check that the link will be opened in a new tab
-  })
+    cy.get(sign_in_page.cypress_copyright)
+      .should("have.attr", "href", "https://cypress.io")
+      .and("have.attr", "target", "_blank"); //check that the link will be opened in a new tab
+  });
 
   it.skip("should open cypress.io link by clicking on cypress copyright", () => {
-    cy.get(sign_in_page.cypress_copyright).should('have.attr', 'href', 'https://cypress.io')
-      .and('have.attr', 'target', '_blank') //check that the link will be opened in a new tab
-      .invoke('removeAttr', 'target').click() //remove the attribute and open the link in the current tab instead of new one
+    cy.get(sign_in_page.cypress_copyright)
+      .should("have.attr", "href", "https://cypress.io")
+      .and("have.attr", "target", "_blank") //check that the link will be opened in a new tab
+      .invoke("removeAttr", "target")
+      .click(); //remove the attribute and open the link in the current tab instead of new one
     //this will fail anyway, because cypress don't support redirection between different superdomains
     //"chromeWebSecurity: false" could be added to cypress config as workaround
-    cy.url().should('eq', 'https://www.cypress.io/')
-  })
-
-})
+    cy.url().should("eq", "https://www.cypress.io/");
+  });
+});
--- cypress/selectors/sign_up_page.js
+++ cypress/selectors/sign_up_page.js
@@ -1,19 +1,27 @@
 export const sign_up_page = {
-    firstName_field: '#firstName',
-    lastName_field: '#lastName',
-    username_field: '#username',
-    password_field: '#password',
-    confirmPassword_field: '#confirmPassword',
-    firstName_validation_message: '#firstName-helper-text',
-    lastName_validation_message: '#lastName-helper-text',
-    username_validation_message: '#username-helper-text',
-    password_validation_message: '#password-helper-text',
-    confirmPassword_validation_message: '#confirmPassword-helper-text',
-    signUp_button: '[data-test="signup-submit"]',
-    generateUsername() {
-        const names = ['Alex', 'Viktor', 'Ivan', 'Ostap', 'Igor', 'Michael', 'nagibator2007']
-        const randomNum = Math.floor(Math.random() * 1000)
-        const pickedNameIndex = Math.floor(Math.random() * names.length)
-        return `${names[pickedNameIndex]}${randomNum}`
-    }
-}
+  firstName_field: "#firstName",
+  lastName_field: "#lastName",
+  username_field: "#username",
+  password_field: "#password",
+  confirmPassword_field: "#confirmPassword",
+  firstName_validation_message: "#firstName-helper-text",
+  lastName_validation_message: "#lastName-helper-text",
+  username_validation_message: "#username-helper-text",
+  password_validation_message: "#password-helper-text",
+  confirmPassword_validation_message: "#confirmPassword-helper-text",
+  signUp_button: '[data-test="signup-submit"]',
+  generateUsername() {
+    const names = [
+      "Alex",
+      "Viktor",
+      "Ivan",
+      "Ostap",
+      "Igor",
+      "Michael",
+      "nagibator2007",
+    ];
+    const randomNum = Math.floor(Math.random() * 1000);
+    const pickedNameIndex = Math.floor(Math.random() * names.length);
+    return `${names[pickedNameIndex]}${randomNum}`;
+  },
+};
