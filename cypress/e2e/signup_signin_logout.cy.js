c0fe31c5521a750a4ed9fd5df2a024aa581b39d7
Oleksii <oleksii.kharchenko@devico.io>
2022-07-25 10:57:33 +0300

    resolve PR comments

--- cypress/e2e/signup_signin_logout.cy.js
+++ cypress/e2e/signup_signin_logout.cy.js
@@ -6,26 +6,31 @@ import { sign_up_page } from "../selectors/sign_up_page";
 describe('Sign-in, sign-up, logout checks', () => {
     const userName = sign_up_page.generateUsername()
     const password = 'RestTest1!'
+
     before('Visit sign-up page', () => {
         cy.visit('/')
         cy.get(sign_in_page.signUp_link).click()
         cy.url().should('contain', '/signup')
     })
+
     it('should show validation errors for first name field', () => {
         cy.get(sign_up_page.firstName_validation_message).should('not.exist')
         cy.get(sign_up_page.firstName_field).click().blur()
         cy.get(sign_up_page.firstName_validation_message).should('be.visible').and('have.text', 'First Name is required')
     })
+
     it('should show validation errors for last name field', () => {
         cy.get(sign_up_page.lastName_validation_message).should('not.exist')
         cy.get(sign_up_page.lastName_field).click().blur()
         cy.get(sign_up_page.lastName_validation_message).should('be.visible').and('have.text', 'Last Name is required')
     })
+
     it('should show validation errors for username field', () => {
         cy.get(sign_up_page.username_validation_message).should('not.exist')
         cy.get(sign_up_page.username_field).click().blur()
         cy.get(sign_up_page.username_validation_message).should('be.visible').and('have.text', 'Username is required')
     })
+
     it('should show validation errors for password field', () => {
         cy.get(sign_up_page.password_validation_message).should('not.exist')
         cy.get(sign_up_page.password_field).click().blur()
@@ -34,6 +39,7 @@ describe('Sign-in, sign-up, logout checks', () => {
         cy.get(sign_up_page.password_validation_message).should('be.visible').and('have.text', 'Password must contain at least 4 characters')
         cy.get(sign_up_page.password_field).clear()
     })
+
     it('should show validation errors for password confirmation field', () => {
         cy.get(sign_up_page.confirmPassword_validation_message).should('not.exist')
         cy.get(sign_up_page.confirmPassword_field).click().blur()
@@ -42,6 +48,7 @@ describe('Sign-in, sign-up, logout checks', () => {
         cy.get(sign_up_page.confirmPassword_validation_message).should('be.visible').and('have.text', 'Password does not match')
         cy.get(sign_up_page.confirmPassword_field).clear()
     })
+
     it('should allow user to sign up', () => {
         cy.intercept("POST", "/users").as("signup")
         cy.get(sign_up_page.firstName_field).type('Oliver').should('have.value', 'Oliver')
@@ -53,6 +60,7 @@ describe('Sign-in, sign-up, logout checks', () => {
         cy.get(sign_up_page.signUp_button).should('be.enabled')
             .click().wait('@signup').its('response.statusCode').should('eq', 201)
     })
+
     it('should allow user to sign in to newly created account with valid credentials', () => {
         cy.intercept('POST', '/login').as('signin')
         cy.visit('/')
@@ -62,6 +70,7 @@ describe('Sign-in, sign-up, logout checks', () => {
             .click().wait('@signin').its('response.statusCode').should('eq', 200)
         cy.get(main_page.getStarted_window).should('be.visible').and('contain.text', 'Get Started with Real World App')
     })
+
     it('should allow user to complete onboarding flow', () => {
         cy.get(main_page.getStartedNext_button).click()
         cy.get(main_page.onboarding_bankName_field).type('Big Bank').should('have.value', 'Big Bank')
@@ -72,17 +81,20 @@ describe('Sign-in, sign-up, logout checks', () => {
         cy.get(main_page.getStartedNext_button).click()
         cy.get(main_page.getStarted_window).should('not.exist')
     })
+
     it('should allow a visitor to logout', () => {
         cy.intercept('POST', '/logout').as('logout')
         cy.get(main_page.logout_button).click()
         cy.url().should('contain', 'signin')
     })
+
     it('should display login errors while logging with invalid credentials', () => {
         cy.get(sign_in_page.username_field).type('oliversykes')
         cy.get(sign_in_page.password_field).type('asdasd')
         cy.get(sign_in_page.signIn_button).click()
         cy.get(sign_in_page.signIn_error).should('be.visible').and('have.text', 'Username or password is invalid')
     })
+
     it('should show error when inputted password is less than 4 symbols', () => {
         cy.get(sign_in_page.username_field).type('oliversykes')
         cy.get(sign_in_page.password_field).type('123').blur()
