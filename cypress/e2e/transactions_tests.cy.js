7dc7a56bbeb412b6c925b9246291eb4aebce5ecf
Oleksii Kharchenko <oleksii.kharchenko@devico.io>
2022-07-28 22:36:10 +0300

    add two cases

--- cypress/e2e/transactions_tests.cy.js
+++ cypress/e2e/transactions_tests.cy.js
@@ -3,9 +3,9 @@ import { main_page } from "../selectors/main_page";
 import { transactions } from "../selectors/transactions";
 
 describe("transactions tests", () => {
-  const userName = "Giovanna74";
+  const userName = "Allie2";
   const password = "s3cret";
-  const transactionAmount = "123";
+  const transactionAmount = "5";
   const noteText = "I'll have two number 9s, number 9 large";
 
   beforeEach("signin", () => {
@@ -13,10 +13,10 @@ describe("transactions tests", () => {
     cy.intercept("POST", "/transactions").as("createTransaction");
     cy.signin_ui(userName, password);
     cy.get(main_page.newTransaction_button).click();
+    cy.wait("@getUsers");
   });
 
   it("navigates to the new transaction form, selects a user and submits a transaction payment", () => {
-    cy.wait("@getUsers").its("response.statusCode").should("eq", 200);
     cy.get(transactions.contacts_list)
       .should("be.visible")
       .contains("Edgar Johns")
@@ -26,16 +26,48 @@ describe("transactions tests", () => {
       "Edgar Johns"
     );
     cy.get(transactions.amount_field)
-      .type("123")
+      .type(transactionAmount)
       .should("contain.value", transactionAmount);
     cy.get(transactions.note_field)
       .type(noteText)
       .should("contain.value", noteText);
     cy.get(transactions.pay_button).click();
     cy.wait("@createTransaction").its("response.statusCode").should("eq", 200);
-    cy.get(transactions.transaction_success_message)
+  });
+
+  it("navigates to the new transaction form, selects a user and submits a transaction request", () => {
+    cy.get(transactions.contacts_list)
       .should("be.visible")
-      .and("contain.text", transactionAmount)
-      .and("contain.text", noteText);
+      .contains("Edgar Johns")
+      .click();
+    cy.get(transactions.selected_contact_title).should(
+      "have.text",
+      "Edgar Johns"
+    );
+    cy.get(transactions.amount_field)
+      .type(transactionAmount)
+      .should("contain.value", transactionAmount);
+    cy.get(transactions.note_field)
+      .type(noteText)
+      .should("contain.value", noteText);
+    cy.get(transactions.request_button).click();
+    cy.wait("@createTransaction").its("response.statusCode").should("eq", 200);
+  });
+
+  it("displays new transaction errors", () => {
+    cy.get(transactions.contacts_list)
+      .should("be.visible")
+      .contains("Edgar Johns")
+      .click();
+    cy.get(transactions.amount_field).clear().blur();
+    cy.get(transactions.note_field).clear().blur();
+    cy.get(transactions.amount_validation_message)
+      .should("be.visible")
+      .and("have.text", "Please enter a valid amount");
+    cy.get(transactions.note_validation_message)
+      .should("be.visible")
+      .and("have.text", "Please enter a note");
+    cy.get(transactions.pay_button).should("be.disabled");
+    cy.get(transactions.request_button).should("be.disabled");
   });
 });
--- cypress/selectors/transactions.js
+++ cypress/selectors/transactions.js
@@ -3,9 +3,10 @@ export const transactions = {
   selected_contact_title:
     ".MuiBox-root > .MuiGrid-container > :nth-child(2) > .MuiTypography-root",
   amount_field: "#amount",
+  amount_validation_message: "#transaction-create-amount-input-helper-text",
   note_field: "#transaction-create-description-input",
+  note_validation_message: "#transaction-create-description-input-helper-text",
   request_button: '[data-test="transaction-create-submit-request"]',
   pay_button: '[data-test="transaction-create-submit-payment"]',
-  transaction_success_message:
-    ".MuiBox-root-67 > .MuiGrid-container > .MuiGrid-root > .MuiTypography-root",
+  user_balance: '[data-test="sidenav-user-balance"]',
 };
