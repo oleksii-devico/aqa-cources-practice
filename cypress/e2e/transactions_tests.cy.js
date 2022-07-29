7ab1eb8dc3ee8b6c71466685d0d58763e9b7fb21
Oleksii Kharchenko <oleksii.kharchenko@devico.io>
2022-07-29 14:22:01 +0300

    add one case + functions

--- cypress/e2e/transactions_tests.cy.js
+++ cypress/e2e/transactions_tests.cy.js
@@ -2,56 +2,31 @@
 import { main_page } from "../selectors/main_page";
 import { transactions } from "../selectors/transactions";
 
-describe("transactions tests", () => {
+const transactionAmount = "5";
+const noteText = "I'll have two number 9s, number 9 large";
+
+describe("User is able to create transactions", () => {
   const userName = "Allie2";
   const password = "s3cret";
-  const transactionAmount = "5";
-  const noteText = "I'll have two number 9s, number 9 large";
 
   beforeEach("signin", () => {
     cy.intercept("GET", "/users").as("getUsers");
     cy.intercept("POST", "/transactions").as("createTransaction");
+    cy.intercept("GET", "/checkAuth").as("checkAuth");
     cy.signin_ui(userName, password);
     cy.get(main_page.newTransaction_button).click();
-    cy.wait("@getUsers");
+  });
+
+  after("logout", () => {
+    cy.logout_ui();
   });
 
   it("navigates to the new transaction form, selects a user and submits a transaction payment", () => {
-    cy.get(transactions.contacts_list)
-      .should("be.visible")
-      .contains("Edgar Johns")
-      .click();
-    cy.get(transactions.selected_contact_title).should(
-      "have.text",
-      "Edgar Johns"
-    );
-    cy.get(transactions.amount_field)
-      .type(transactionAmount)
-      .should("contain.value", transactionAmount);
-    cy.get(transactions.note_field)
-      .type(noteText)
-      .should("contain.value", noteText);
-    cy.get(transactions.pay_button).click();
-    cy.wait("@createTransaction").its("response.statusCode").should("eq", 200);
+    transactions.createPayTransaction(transactionAmount, noteText);
   });
 
   it("navigates to the new transaction form, selects a user and submits a transaction request", () => {
-    cy.get(transactions.contacts_list)
-      .should("be.visible")
-      .contains("Edgar Johns")
-      .click();
-    cy.get(transactions.selected_contact_title).should(
-      "have.text",
-      "Edgar Johns"
-    );
-    cy.get(transactions.amount_field)
-      .type(transactionAmount)
-      .should("contain.value", transactionAmount);
-    cy.get(transactions.note_field)
-      .type(noteText)
-      .should("contain.value", noteText);
-    cy.get(transactions.request_button).click();
-    cy.wait("@createTransaction").its("response.statusCode").should("eq", 200);
+    transactions.createRequestTransaction(transactionAmount, noteText);
   });
 
   it("displays new transaction errors", () => {
@@ -69,5 +44,52 @@ describe("transactions tests", () => {
       .and("have.text", "Please enter a note");
     cy.get(transactions.pay_button).should("be.disabled");
     cy.get(transactions.request_button).should("be.disabled");
+  });
+});
+
+describe("User is able to receive pay and request transactions", () => {
+  const payerUserName = "Allie2";
+  const receiverUserName = "Katharina_Bernier";
+  const password = "s3cret";
+
+  beforeEach("signin", () => {
+    cy.intercept("GET", "/users").as("getUsers");
+    cy.intercept("POST", "/transactions").as("createTransaction");
+    cy.intercept("GET", "/checkAuth").as("checkAuth");
+  });
+
+  /*afterEach("logout", () => {
+    cy.logout_ui();
+  });*/
+
+  it("submits a transaction payment and verifies the deposit for the receiver", () => {
+    let payerStartBalance, receiverStartBalance;
+
+    cy.signin_ui(receiverUserName, password);
+    cy.get(transactions.user_balance)
+      .invoke("text")
+      .then((x) => {
+        receiverStartBalance = x; // something like "$1,484.81"
+        expect(receiverStartBalance).to.match(/\$\d/);
+      });
+    cy.logout_ui();
+    cy.signin_ui(payerUserName, password);
+    cy.get(transactions.user_balance)
+      .invoke("text")
+      .then((x) => {
+        payerStartBalance = x; // something like "$1,484.81"
+        expect(payerStartBalance).to.match(/\$\d/);
+      });
+    cy.get(main_page.newTransaction_button).click();
+    transactions.createPayTransaction(transactionAmount, noteText);
+    cy.get(transactions.user_balance).should(($el) => {
+      expect($el.text()).to.not.equal(payerStartBalance);
+    });
+    cy.logout_ui();
+    cy.signin_ui(receiverUserName, password);
+    cy.url().should("not.contain", "/signin");
+    cy.get(transactions.user_balance).should(($el) => {
+      expect($el.text()).to.not.equal(receiverStartBalance);
+    });
   });
 });
--- cypress/selectors/transactions.js
+++ cypress/selectors/transactions.js
@@ -9,4 +9,44 @@ export const transactions = {
   request_button: '[data-test="transaction-create-submit-request"]',
   pay_button: '[data-test="transaction-create-submit-payment"]',
   user_balance: '[data-test="sidenav-user-balance"]',
+  createPayTransaction(transactionAmount, noteText) {
+    cy.wait("@getUsers");
+    cy.get(transactions.contacts_list)
+      .should("be.visible")
+      .contains("Edgar Johns")
+      .click();
+    cy.get(transactions.selected_contact_title).should(
+      "have.text",
+      "Edgar Johns"
+    );
+    cy.get(transactions.amount_field)
+      .type(transactionAmount)
+      .should("contain.value", transactionAmount);
+    cy.get(transactions.note_field)
+      .type(noteText)
+      .should("contain.value", noteText);
+    cy.get(transactions.pay_button).click();
+    cy.wait("@createTransaction").its("response.statusCode").should("eq", 200);
+    cy.wait("@checkAuth");
+  },
+  createRequestTransaction(transactionAmount, noteText) {
+    cy.wait("@getUsers");
+    cy.get(transactions.contacts_list)
+      .should("be.visible")
+      .contains("Edgar Johns")
+      .click();
+    cy.get(transactions.selected_contact_title).should(
+      "have.text",
+      "Edgar Johns"
+    );
+    cy.get(transactions.amount_field)
+      .type(transactionAmount)
+      .should("contain.value", transactionAmount);
+    cy.get(transactions.note_field)
+      .type(noteText)
+      .should("contain.value", noteText);
+    cy.get(transactions.request_button).click();
+    cy.wait("@createTransaction").its("response.statusCode").should("eq", 200);
+    cy.wait("@checkAuth");
+  },
 };
